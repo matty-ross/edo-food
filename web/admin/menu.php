379827b8b672625e68b2ee54974093eea39de0d4
Matty <54020396+matty-ross@users.noreply.github.com>
2022-09-27 20:26:19 +0200

    Add files via upload

--- web/admin/menu.php
+++ web/admin/menu.php
@@ -10,28 +10,82 @@ $menu_main_dishes = $db->get_menu_items(null, 'main_dish');
 
 ?>
 <hr>
+<div>
+    <input type="date">
+</div>
+<hr>
+<div id="add-menu-item-form">
+    <h2>Pridať položku do menu</h2>
+    <div>
+        <select id="meal-id">
+<?php
+
+foreach ($soups as $soup)
+{
+    echo("<option value=\"{$soup['id']}\">{$soup['name']}</option>\n");
+}
+foreach ($main_dishes as $main_dish)
+{
+    echo("<option value=\"{$main_dish['id']}\">{$main_dish['name']}</option>\n");
+}
+
+?>
+        </select>
+    </div>
+</div>
+<hr>
 <table>
     <caption>Prehľad polievok v menu</caption>
     <thead>
         <tr>
+            <th>#</th>
             <th>Názov polievky</th>
+            <th>Objem</th>
+            <th>Cena</th>
         </tr>
     </thead>
     <tbody>
 <?php
 
+$i = 1;
 foreach ($menu_soups as $menu_soup)
 {
     echo("<tr>\n");
 
-    echo("<td>\n");
-    echo("<select>\n");
-    foreach ($soups as $soup)
-    {
-        echo("<option value=\"{$soup['id']}\">{$soup['name']}</option>\n");
-    }
-    echo("</select>\n");
-    echo("</td>\n");
+    echo("<td>$i</td>\n");
+    echo("<td>{$menu_soup['meal_name']}</td>\n");
+    echo("<td>{$menu_soup['meal_amount']} l</td>\n");
+    echo("<td>{$menu_soup['meal_price']} &euro;</td>\n");
+
+    echo("<tr>\n");
+}
+
+?>
+    </tbody>
+</table>
+<hr>
+<table>
+    <caption>Prehľad hlavných jedál v menu</caption>
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Názov hlavného jedla</th>
+            <th>Hmotnosť</th>
+            <th>Cena</th>
+        </tr>
+    </thead>
+    <tbody>
+<?php
+
+$i = 1;
+foreach ($menu_main_dishes as $menu_main_dish)
+{
+    echo("<tr>\n");
+
+    echo("<td>$i</td>\n");
+    echo("<td>{$menu_main_dish['meal_name']}</td>\n");
+    echo("<td>{$menu_main_dish['meal_amount']} g</td>\n");
+    echo("<td>{$menu_main_dish['meal_price']} &euro;</td>\n");
 
     echo("<tr>\n");
 }
